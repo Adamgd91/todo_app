4a7d46b035aa2d54775dc431342adad3a7fff223
Adam Davidson <agdavidson91@gmail.com>
2026-02-25 15:46:38 -0500

    password

--- testingground.py
+++ testingground.py
@@ -6,11 +6,13 @@ attempts = 1
 
 while password != "pass123" and attempts < 5:
     attempts += 1
-    print(f'Incorrect Password and this is your {attempts} attempt. You have {5 - attempts} more to go!')
+    print(
+        f"Incorrect Password and this is your {attempts} attempt. You have {5 - attempts} more to go!"
+    )
     password = input("What is your password?")
-    
-    
+
+
 if password == "pass123":
-    print(f'Password correct')
+    print(f"Password correct")
 else:
     print("You failed")
