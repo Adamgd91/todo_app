dd5342da032c85ad1424c4dfe50d41649bf754a7
Adam Davidson <agdavidson91@gmail.com>
2026-03-01 06:03:37 -0500

    fixed todo

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-user_prompt = input("Enter a todo item: ")
-
 list_todos = []
 
 while True:
-    todo = input(user_prompt)
-    list_todos.append(todo)
-    print(list_todos)
+    user_action = input("Type add or show Todos: ")
+
+    match user_action:
+        case "add":
+            todo = input("Enter a Todo: ")
+            list_todos.append(todo)
+        case "show":
+            print(list_todos)
